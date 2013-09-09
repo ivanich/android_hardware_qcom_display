2746d9674dee731528f588c148e8a5c93e0f3188
Steve Kondik <shade@chemlab.org>
2013-09-09 14:16:32 -0700

    gralloc: Fix some differences with compatibility code
    Change-Id: I0f41ff987b6b3f554e11ea2f259d778c8cee7fa2
    
    Conflicts:
    	libgralloc/alloc_controller.cpp

--- libgralloc/alloc_controller.cpp
+++ libgralloc/alloc_controller.cpp
@@ -188,7 +188,9 @@ int IonController::allocate(alloc_data& data, int usage)
 {
     int ionFlags = 0;
     int ret;
+#ifndef SECURE_MM_HEAP
     bool nonContig = false;
+#endif
 
     data.uncached = useUncached(usage);
     data.allocType = 0;
@@ -198,13 +200,17 @@ int IonController::allocate(alloc_data& data, int usage)
 
     if(usage & GRALLOC_USAGE_PRIVATE_SYSTEM_HEAP) {
         ionFlags |= ION_HEAP(ION_SYSTEM_HEAP_ID);
+#ifndef SECURE_MM_HEAP
         nonContig = true;
+#endif
     }
 
 #ifndef NO_IOMMU
     if(usage & GRALLOC_USAGE_PRIVATE_IOMMU_HEAP) {
         ionFlags |= ION_HEAP(ION_IOMMU_HEAP_ID);
+#ifndef SECURE_MM_HEAP
         nonContig = true;
+#endif
     }
 #endif
 
@@ -269,13 +275,15 @@ int IonController::allocate(alloc_data& data, int usage)
     {
         ALOGW("Falling back to system heap");
         data.flags = ION_HEAP(ION_SYSTEM_HEAP_ID);
+#ifndef SECURE_MM_HEAP
         nonContig = true;
+#endif
         ret = mIonAlloc->alloc_buffer(data);
     }
 
     if(ret >= 0 ) {
         data.allocType |= private_handle_t::PRIV_FLAGS_USES_ION;
-        if(nonContig)
+        if (nonContig)
             data.allocType |= private_handle_t::PRIV_FLAGS_NONCONTIGUOUS_MEM;
 #ifndef SECURE_MM_HEAP
         if(ionFlags & ION_SECURE)
